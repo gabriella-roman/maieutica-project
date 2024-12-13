fd1d54b18952a6668545192f769cf84e5f69aa6f
Gabriella Roman <gabriellaperniconi@gmail.com>
2024-12-13 19:19:05 -0300

    Add Header component and css

--- maieutica-web/src/components/Header/Header.tsx
+++ maieutica-web/src/components/Header/Header.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import styles from './Header.module.css'
+import logo from '../../assets/images/logo-maieutica.svg'
+import arrow from '../../assets/icons/arrow.svg'
+import { useNavigate } from 'react-router-dom'
 
 export function Header() {
+  const navigate = useNavigate()
+
   return (
-    <header>
-      <p>Header</p>
-    </header>
+    <div className={styles.header}>
+      <img src={logo} alt='Logo-Maieutica' />
+
+      <div className={styles.links}>
+        <a className={styles.text} onClick={() => navigate('/')}>Home</a>
+        <a className={styles.text} onClick={() => navigate('/about-us')}>Sobre nós</a>
+        <a className={styles.text} onClick={() => navigate('/contact-us')}>Fale conosco</a>
+        <a className={styles.text} onClick={() => navigate('/our-services')}>Nossos serviços</a>
+
+        <button
+          className={styles.button}
+          onClick={() => navigate('/job-board')}
+        >
+          Ver vagas
+
+          <img src={arrow} alt='Arrow' />
+        </button>
+      </div>
+    </div>
   )
 }
