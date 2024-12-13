d0db85b09ea352602237563cc318261a10ba4d3d
Gabriella Roman <gabriellaperniconi@gmail.com>
2024-12-13 18:00:12 -0300

    Add react-router-dom for routing and update routes structure

--- maieutica-web/src/routes/routes.tsx
+++ maieutica-web/src/routes/routes.tsx
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import React from 'react'
-import AboutUs from '../pages/AboutUs/AboutUs'
-import ContactUs from '../pages/ContactUs/ContactUs'
-import Faq from '../pages/FAQ/Faq'
-import Home from '../pages/Home/Home'
-import JobBoard from '../pages/JobBoard/JobBoard'
-import OurServices from '../pages/OurServices/OurServices'
+import Home  from '../pages/Home/Home'
+import AboutUs  from '../pages/AboutUs/AboutUs'
+import ContactUs  from '../pages/ContactUs/ContactUs'
+import Faq  from '../pages/Faq/Faq'
+import JobBoard  from '../pages/JobBoard/JobBoard'
+import OurServices  from '../pages/OurServices/OurServices'
 
 export function AppRoutes() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/faq" element={<Faq />} />
-        <Route path="/" element={<Home />} />
         <Route path="/job-board" element={<JobBoard />} />
         <Route path="/our-services" element={<OurServices />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
